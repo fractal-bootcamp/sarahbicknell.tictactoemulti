febf1368cc18a5f93e24deca60e64943d49e73b0
sarahbicknell <166096006+sarahbicknell@users.noreply.github.com>
2024-06-14 16:53:21 -0400

    app now renders client side and can make moves and refresh

--- server.ts
+++ server.ts
@@ -1,61 +1,93 @@
 import express from "express";
 import cors from "cors";
-
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 //json handling
 app.use(express.json());
 app.use(cors())
 
+//INITIALIZE BOARD
 const emptyBoard = ["", "", "", "", "", "", "", "", ""];
 
-
+// HARDCODED GAME FOR TESTING 
 let games = {
  ["12345"]:{
     gameId: "12345",
     board: emptyBoard,
     currentPlayer: "X", 
-    player1: {token: "x", id: ""},
-    player2: {token: "x", id: ""},
+    player1: {token: "X", id: ""},
+    player2: {token: "O", id: ""},
     winState: {outcome: null, winner: null}
  }
 }
 
+//GET FOR BASIC ROUTE 
 app.get('/', (req, res) => {
     res.send("hi hoes")
 })
 
+//get a game by ID as a parameter
 app.get('/game/:id', (req, res) => {
+    // assign id to the url parameter 
     const id = req.params.id;
+    // get specific game by ID
     const game = games[id];
 
     // if no game found
     if (!game) {
         return res.status(404).send("game not found")
     }
-
+    //send json of the game object
     res.json({game: game})
 
 })
 
+//POST request for making a move in the game 
 app.post('/game/:id/move', (req, res) => {
     const id = req.params.id;
     const game = games[id];
 
-    const {index} = req.body
+    //variable holds the index of the square clicked 
+    const squareIndex = req.body.square;
 
     // if no game found
     if (!game) {
         return res.status(404).send("game not found")
     }
 
-    const newBoard = game.board; 
+    //sets new board and switches current player 
+    const newBoard = game.board.slice(); 
     const player = game.currentPlayer
-    newBoard[index] = player; 
-    game.currentPlayer = player === "x" ? "o" : "x"
+    newBoard[squareIndex] = player; 
+    game.currentPlayer = player === "X" ? "O" : "X"
+    game.board = newBoard
+
+    //returns game json object
+    res.json({game})
 
 })
 
+app.post('/game/:id/reset', (req, res) => { 
+    const id = req.params.id; 
+    const game = games[id];
+
+    game.board = [...emptyBoard]
+    game.currentPlayer = "X";
+    game.winState = {outcome: null, winner: null}
+
+
+    res.json({game})
+    console.log("Reset Post request: ", game)
+
+
+})
+
+//runs the server on port 4000 
 app.listen(4000, () => {
     console.log("listening on port 4000")
 
--- src/App.tsx
+++ src/App.tsx
@@ -22,52 +22,68 @@ function Square({value, onSquareClick}:SquareProps) {
   return (
     <button className="square" onClick={onSquareClick} > {displayValue} </button> 
   )
-
 }
-interface ResetButtonProps {
-  onResetClick: () => void
-}
+// interface ResetButtonProps {
+//   onResetClick: () => void
+// }
 
-function ResetButton({onResetClick}:ResetButtonProps) {
+function ResetButton({onResetClick}) {
   return (
     <button className="reset" onClick={onResetClick} > Reset </button>
   )
 }
 
+
 // we need to beable to get the game to the server
 const serverPath = "http://localhost:4000"
 
+const resetGame = async (id: string) => {
+  const response = await fetch(`${serverPath}/game/${id}/reset`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const json = await response.json();
+  return json;
+};
+
 const getGame = async (id: string) => {
-  const response = await fetch(`${serverPath}/gmae/${id}`)
+  const response = await fetch(`${serverPath}/game/${id}`)
   const json = await response.json(); 
-  console.log(json)
-  return(json)
+  return json;
 }
 
-const makeAMove = async (id: string) => {
-  const response = await fetch(`${serverPath}/gmae/${id}`)
+const makeAMove = async (id: string, squareIndex:number) => {
+  const response = await fetch(`${serverPath}/game/${id}/move`, {
+    method: "POST",
+    body: JSON.stringify({square: squareIndex}),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
   const json = await response.json(); 
-  console.log(json)
-  return(json)
-}//make this set post req to server
+  return json
+}
 
 //in the lobby, when you create a game you should store the id, and if you join also store id
 
 
-const initialBoard:Board = Array(9).fill("")
+// const initialBoard:Board = Array(9).fill("")
 
 
 export default function GameBoard() {
   // const [nextPlayer, setNextPlayer] = useState(true)
   // const [squares, setSquares] = useState(initialBoard)
+  const gameId = "12345"
   const [game, setGame] = useState(null);
   const [poller, setPoller] = useState(0);
 
+  // this i think is supposed to referesh the page so the other players data renders 
   useEffect(() => {
     const initializeGame = async () => {
       // Go get the game
       const data = await getGame(gameId);
-
       // store the game in state
       setGame(data.game);
     };
@@ -81,73 +97,132 @@ export default function GameBoard() {
     }, 1000);
   }, [poller]);
 
-
-  function handleClick(i: number) { 
-    if (squares[i] || winner) {
-      return
+  
+  const handleSquareClick = async (index) => {
+    if (game.board[index] || game.winState.winner) {
+      return;
     }
-    else {
-      const nextSquares = squares.slice();
-      if (nextPlayer) {
-      nextSquares[i] = "X" ;
-      } else {
-      nextSquares[i] = "O";
-      }
-      setNextPlayer(!nextPlayer)
-      setSquares(nextSquares) }
-  }
-  
-  function resetGame() { 
-    setSquares(Array(9).fill(""))
-    setNextPlayer(true)
+    const updatedGame = await makeAMove(gameId, index);
+    setGame(updatedGame.game);
+  };
+
+  const handleReset = async () => {
+    const updatedGame = await resetGame(gameId);
+    setGame(updatedGame.game);
+  };
+
+  if (!game) {
+    return <div>Loading...</div>;
   }
 
-  const winState = checkWinCondition({board: squares})
-  const winner = winState.winner
-  let winnerName;
-  if (winner == "X"){
-    winnerName = "Tank"
-  } else if (winner == "O") {
-    winnerName = "Toe"
-  }
-  let status; 
-  if (winner) {
-    status = (winnerName=="Tank"? "🎉🪖" : "🎉😼" ) + winnerName + " wins!" + (winnerName=="Tank"? "🪖🎉" : "😼🎉" )
-  } else if (winState.outcome == "draw") {
-    status = "💥😾Git gud noobs!😾💥"
+  let status;
+  if (game.winState.winner) {
+    status = `Winner: ${game.winState.winner}`;
   } else {
-    //i should update this to be more clear probably 
-    status = (nextPlayer ? "Tank" : "Toe") + " goes next" + (nextPlayer? "🪖" : "😼" )
+    status = `Next Player: ${game.currentPlayer}`;
   }
-  
 
-  return (
-    <> 
-    <div className="page">
-      <div className="game">
-        <h1 className="title "> Pink Tank Toe !</h1>
-        <div className="status"> {status} </div>
-        <div>
-          <div className="board-row">
-            <Square value={squares[0]} onSquareClick={() => handleClick(0)} /> 
-            <Square value={squares[1]} onSquareClick={() => handleClick(1)}/> 
-            <Square value={squares[2]} onSquareClick={() => handleClick(2)}/> 
-          </div> 
-          <div className="board-row"> 
-            <Square value={squares[3]} onSquareClick={() => handleClick(3)}/> 
-            <Square value={squares[4]} onSquareClick={() => handleClick(4)}/> 
-            <Square value={squares[5]} onSquareClick={() => handleClick(5)}/> 
-          </div> 
-          <div className="board-row"> 
-            <Square value={squares[6]} onSquareClick={() => handleClick(6)}/> 
-            <Square value={squares[7]} onSquareClick={() => handleClick(7)}/> 
-            <Square value={squares[8]} onSquareClick={() => handleClick(8)}/> 
-            
-          </div>
-        </div>
-          <ResetButton onResetClick={resetGame} /> 
+
+
+  // function handleClick(i: number) { 
+  //   if (squares[i] || winner) {
+  //     return
+  //   }
+  //   else {
+  //     const nextSquares = squares.slice();
+  //     if (nextPlayer) {
+  //     nextSquares[i] = "X" ;
+  //     } else {
+  //     nextSquares[i] = "O";
+  //     }
+  //     setNextPlayer(!nextPlayer)
+  //     setSquares(nextSquares) }
+  // }
+
+  // const winState = checkWinCondition({board: squares})
+  // const winner = winState.winner
+  // let winnerName;
+  // if (winner == "X"){
+  //   winnerName = "Tank"
+  // } else if (winner == "O") {
+  //   winnerName = "Toe"
+  // }
+  // let status; 
+  // if (winner) {
+  //   status = (winnerName=="Tank"? "🎉🪖" : "🎉😼" ) + winnerName + " wins!" + (winnerName=="Tank"? "🪖🎉" : "😼🎉" )
+  // } else if (winState.outcome == "draw") {
+  //   status = "💥😾Git gud noobs!😾💥"
+  // } else {
+  //   //i should update this to be more clear probably 
+  //   status = (nextPlayer ? "Tank" : "Toe") + " goes next" + (nextPlayer? "🪖" : "😼" )
+  // }
+  
+return (
+    <div>
+      <div>
+        <button onClick={() => getGame("12345")}>Get Game</button>
+        {JSON.stringify(game)}
+      </div>
+      {/* <div>
+        {game?.board.map((square, index) => (
+                <button
+                  key={index}
+                  className="square"
+                  onClick={() => makeAMove(gameId, index)}
+                >
+                  {square} 1
+                </button>
+              ))}
+      </div> */}
+              
+        <div className="game"> 
+          <div className="status"> {status} </div>
+            <div className="board-row">
+              <Square value={game.board[0]} onSquareClick={() => handleSquareClick(0)} /> 
+              <Square value={game.board[1]} onSquareClick={() => handleSquareClick(1)}/> 
+              <Square value={game.board[2]} onSquareClick={() => handleSquareClick(2)}/> 
+            </div> 
+            <div className="board-row"> 
+              <Square value={game.board[3]} onSquareClick={() => handleSquareClick(3)}/> 
+              <Square value={game.board[4]} onSquareClick={() => handleSquareClick(4)}/> 
+              <Square value={game.board[5]} onSquareClick={() => handleSquareClick(5)}/> 
+            </div> 
+            <div className="board-row"> 
+              <Square value={game.board[6]} onSquareClick={() => handleSquareClick(6)}/> 
+              <Square value={game.board[7]} onSquareClick={() => handleSquareClick(7)}/> 
+              <Square value={game.board[8]} onSquareClick={() => handleSquareClick(8)}/> 
+            </div>
+          <div> 
+            <ResetButton onResetClick={handleReset} /> 
+          </div>  
       </div>
     </div>
-    </>
   )
 }
+    //  <div className="page">
+    //   <div className="game">
+    //     <h1 className="title "> Pink Tank Toe !</h1>
+    //     <div className="status"> {status} </div>
+    //     <div>
+    //       <div className="board-row">
+    //         <Square value={squares[0]} onSquareClick={() => handleClick(0)} /> 
+    //         <Square value={squares[1]} onSquareClick={() => handleClick(1)}/> 
+    //         <Square value={squares[2]} onSquareClick={() => handleClick(2)}/> 
+    //       </div> 
+    //       <div className="board-row"> 
+    //         <Square value={squares[3]} onSquareClick={() => handleClick(3)}/> 
+    //         <Square value={squares[4]} onSquareClick={() => handleClick(4)}/> 
+    //         <Square value={squares[5]} onSquareClick={() => handleClick(5)}/> 
+    //       </div> 
+    //       <div className="board-row"> 
+    //         <Square value={squares[6]} onSquareClick={() => handleClick(6)}/> 
+    //         <Square value={squares[7]} onSquareClick={() => handleClick(7)}/> 
+    //         <Square value={squares[8]} onSquareClick={() => handleClick(8)}/> 
+            
+    //       </div>
+    //     </div>
+    //       <ResetButton onResetClick={resetGame} /> 
+    //   </div>
+    // </div>
+    // </> 
+  
